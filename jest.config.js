c3124e017b71bca2da189699071d064a31a10523
shushant-ghosh <shushant.ghosh.857@gmail.com>
2025-05-02 13:52:15 +0530

    excluding index.js from test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    testEnvironment: 'node',
-    setupFilesAfterEnv: ['./jest.setup.js'], 
-  };
-  
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['./jest.setup.js'], 
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "src/index.js"
+  ]
+};
